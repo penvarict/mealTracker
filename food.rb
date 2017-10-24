49010b9e90de16661918f7b13d0be969bb51cd29
penvarict <penvarict@s.dcsdk12.org>
2017-10-24 10:50:17 -0600

    implement food class

---
+++ food.rb
@@ -0,0 +1,29 @@
+class Food
+	attr_accessor :name, :portion_size, :calories, #portions eaten
+
+
+	def initialize(name, portion_size, calories)
+		@name = name
+		@portion_size = portion_size
+		@calories = calories
+
+
+	end
+
+
+	def total_calories (portions_eaten)
+		portions_eaten * calories
+
+	end
+
+	def to_s
+		"#{name}|#{portion_size} | #{calories}"
+
+	end
+
+
+
+
+
+
+end
---
+++ test.rb
@@ -0,0 +1,7 @@
+require './food'
+
+
+scrambled_eggs = Food.new("Scrambled Eggs", "1 egg", 100)
+
+
+puts scrambled_eggs
